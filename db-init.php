be33d66b3d11ef4ee91ab118f6600a26c123bb97
Marc <marcchiapco34@gmail.com>
2021-01-23 01:46:06 +0800

    db

---
+++ db-init.php
@@ -0,0 +1,63 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE store";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+    $dbname = "store";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // sql to create table
+    $sql = "CREATE TABLE users (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        email VARCHAR(50), 
+        pass VARCHAR(255),
+        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+    if ($conn->query($sql) === TRUE) {
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        echo "Table users created successfully";
+        $sql = "CREATE TABLE user_info (
+            infoID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            fullname VARCHAR (255),
+            flrnum INT (10),
+            province VARCHAR (255),
+            municipality VARCHAR (255),
+            barangay VARCHAR (255),
+            mobilenum VARCHAR (15),
+            FOREIGN KEY (infoID) REFERENCES users(id)
+            )";
+            if ($conn->query($sql) === TRUE) {
+                
+                echo "Table user_info created successfully";
+            } else {
+                echo "Error creating table: " . $conn->error;
+            }    
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }    
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
