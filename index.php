56aa21f8118585e06a46d3c5bf96c458f9bfa7d9
Marc <marcchiapco34@gmail.com>
2021-01-24 15:18:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <a href="products.php?itemType=KB">Keyboards</a>
     <a href="products.php?itemType=KC"">Keycaps</a>
     <a href="products.php?itemType=SW"">Switches</a>
+    <a href="products.php?itemType=DM"">Deskmats</a>
 </body>
 
 </html>
