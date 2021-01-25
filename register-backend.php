7dc10833716185d91137b46b8f617a4ffccc5b1c
Marc <marcchiapco34@gmail.com>
2021-01-25 15:45:38 +0800

    Update register-backend.php

--- register-backend.php
+++ register-backend.php
@@ -7,7 +7,8 @@
 	$fname = $signup->escape_string($_POST['fname']);
     $lname = $signup->escape_string($_POST['lname']);
     $email = $signup->escape_string($_POST['email']);
-    $password = $signup->escape_string($_POST['password']);
+	$password = $signup->escape_string($_POST['password']);
+	$password2 = $signup->escape_string($_POST['password2']);
 
 	$check = $signup->emailCheck($email);
 	if($check == true){
@@ -15,13 +16,21 @@
     	header('location:register.php');
 	}
 	else{
-		$success = $signup->insert($fname, $lname, $email, $password);
-		if(!$success){
-			$_SESSION['message'] = 'There seems to be an error.';
-			header('location:register.php');
+		if ($password == $password2)
+		{
+			$success = $signup->insert($fname, $lname, $email, $password);
+			if(!$success){
+				$_SESSION['message'] = 'There seems to be an error.';
+				header('location:register.php');
+			}
+			else{
+				header('location:login.php');
+			}
 		}
-		else{
-			header('location:login.php');
+
+		else
+		{
+			$_SESSION['message'] = 'Password does not match';
 		}
 
 	}
