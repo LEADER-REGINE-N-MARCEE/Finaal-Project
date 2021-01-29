c4d9211724c73e5cd47881ddc0bc5672e5306adb
Marc <marcchiapco34@gmail.com>
2021-01-29 08:30:50 +0800

    Update buynow.php

--- buynow.php
+++ buynow.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include_once('productPage-backend.php');
+include_once('productDB.php');
 
 $usrID = $_SESSION['user'];
 $itemCode = $_SESSION['itemCode'];
@@ -8,7 +8,7 @@ $itemName = $_SESSION['itemName'];
 $itemType = $_SESSION['itemType'];
 $quantity = $_POST['quantity'];
 
-$productsView = new productsView();
+$productsView = new products();
 
 
 $buynow = $productsView->buy($itemCode, $usrID, $itemType, $itemName, $quantity)
